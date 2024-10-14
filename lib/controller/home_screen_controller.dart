599c98c7711b0005e7f527f280e02c2b3bc3221d
abilbabu <abilbabu2019@gamil.com>
2024-10-14 14:57:09 +0530

    finish

--- lib/controller/home_screen_controller.dart
+++ lib/controller/home_screen_controller.dart
@@ -2,7 +2,7 @@ import 'package:sqflite/sqflite.dart';
 
 class HomeScreenController {
   static late Database myDatabase;
- static List<Map> EmployeeDataList =[];
+  static List<Map> EmployeeDataList = [];
   // mydatabase can be used ina a all funct while  declaring it global && late is used to mydatabase not null value in future
   static Future initDb() async {
     // oPen database
@@ -14,20 +14,32 @@ class HomeScreenController {
     });
   }
 
-  static Future addEmployee({required String name,required String designation}) async {
+  static Future addEmployee(
+      {required String name, required String designation}) async {
     await myDatabase.rawInsert(
         'INSERT INTO Employee (name, designation) VALUES(?, ?)',
-        [name,designation]);
+        [name, designation]);
     getAllEmployee();
   }
 
   static Future getAllEmployee() async {
-    EmployeeDataList =
-        await myDatabase.rawQuery('SELECT * FROM Employee');
+    EmployeeDataList = await myDatabase.rawQuery('SELECT * FROM Employee');
     print(EmployeeDataList);
   }
 
-  removeEmployee() {}
+  static Future removeEmployee(int id) async {
+    await myDatabase.rawDelete('DELETE FROM Employee WHERE id = ?', [id]);
+    getAllEmployee();
+  }
 
-  updateEmployee() {}
+  static updateEmployee(
+    String newName,
+    String newdesignation,
+    int id,
+  ) async {
+    await myDatabase.rawUpdate(
+        'UPDATE Employee SET name = ?, designation = ? WHERE id = ?',
+        [newName, newdesignation, id]);
+    getAllEmployee();
+  }
 }
--- lib/view/Home_screen/home_screen.dart
+++ lib/view/Home_screen/home_screen.dart
@@ -30,20 +30,44 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: ListView.separated(
           itemBuilder: (context, index) => ListTile(
+                onTap: () {
+                  _CustomBottomSheet(context,
+                      isedit: true,
+                      currentName: HomeScreenController.EmployeeDataList[index]
+                          ["name"],
+                      currentdesignation: HomeScreenController
+                          .EmployeeDataList[index]['designation'],
+                      Employeeid: HomeScreenController.EmployeeDataList[index]["id"]);
+                }, //editing section
                 title: Text(HomeScreenController.EmployeeDataList[index]["name"]
                     .toString()),
                 subtitle: Text(HomeScreenController.EmployeeDataList[index]
                         ['designation']
-                    .toString()),
+                    .toString()), // add employee section
+                trailing: IconButton(
+                    onPressed: () async {
+                      await HomeScreenController.removeEmployee(
+                              HomeScreenController.EmployeeDataList[index]
+                                  ["id"])
+                          .toString();
+                      setState(() {});
+                    },
+                    icon: Icon(Icons.delete_outline)), //remove section
               ),
           separatorBuilder: (context, index) => Divider(),
           itemCount: HomeScreenController.EmployeeDataList.length),
     );
   }
 
-  Future<dynamic> _CustomBottomSheet(BuildContext context) {
+  Future<dynamic> _CustomBottomSheet(BuildContext context,
+      {bool isedit = false, String? currentName, String? currentdesignation, int? Employeeid}) {
     TextEditingController nameController = TextEditingController();
     TextEditingController designationController = TextEditingController();
+    if (isedit) {
+      nameController.text = currentName??"";
+      designationController.text = currentdesignation??"";
+      
+    }
     return showModalBottomSheet(
       context: context,
       builder: (context) {
@@ -90,10 +114,15 @@ class _HomeScreenState extends State<HomeScreen> {
                     Expanded(
                         child: ElevatedButton(
                             onPressed: () async {
-                              await HomeScreenController.addEmployee(
-                                name: nameController.text,
-                                designation: designationController.text,
-                              );
+
+                              if (isedit = true) {
+                               await HomeScreenController.updateEmployee(nameController.text, designationController.text, Employeeid!);
+                              } else {
+                               await  HomeScreenController.addEmployee(
+                                  name: nameController.text,
+                                  designation: designationController.text,
+                                );
+                              }
                               setState(() {});
                               Navigator.pop(context);
                             },
